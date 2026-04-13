b8af0d8d6dcfb794314515c8566c9063e3ed558c
Pradhyumna Kanathur <pradhyumna2k@gmail.com>
2026-04-13 18:20:30 +0100

    Initial login with keycloak implementation

--- app/app.py
+++ app/app.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from flask import flash, redirect, render_template, request, url_for, session, abort
+from authlib.integrations.flask_client import OAuth
 import os
 
 app=Flask(__name__)
 app.secret_key=os.urandom(24)
+
+#Keycloak configuration
+KEYCLOAK_ISSUER = "http://localhost:8080/admin/master/console/#/master/realms"
+CLIENT_ID = 1
+REDIRECT_URI = "http://localhost:4000/callback"
+
+oauth = OAuth(app)
+
+keycloak = oauth.register(
+    'keycloak',
+    client_id = CLIENT_ID,
+    server_metadata_url = f'{KEYCLOAK_ISSUER}/.well-known/openid-configuration',
+    client_kwargs = {'scope': 'openid profile email'}
+)
+
 @app.route("/")
 def home():
     if not session.get("logged_in"):
@@ -11,16 +27,17 @@ def home():
     else:
         return "Hello There <a href='/logout'>Logout</a>"
 
-@app.route("/login", methods=["POST"])
-def admin_login():
-    username = request.form.get("username")
-    password = request.form.get("password")
+@app.route("/login")
+def login():
+    redirect_uri = url_for('callback', _external=True)
+    return keycloak.authorize_redirect(redirect_uri)
 
-    if username == "admin" and password == "password":
-        session["logged_in"] = True
-    else:
-        flash("Invalid username or password!")
-    return redirect(url_for("home"))
+@app.route("/callback")
+def callback():
+    token = keycloak.authorize_access_token()
+    user = keycloak.parse_id_token(token)
+    session['user'] = user
+    return redirect(url_for('home'))
 
 @app.route("/logout")
 def logout():
